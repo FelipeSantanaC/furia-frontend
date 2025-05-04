fa6854acc871e75a8ec644699cd7a653c5e78fb4
FelipeSantanaC <felipecabocloo@gmail.com>
2025-05-04 12:54:10 -0300

    feat: adicionando spinner aos botões

--- src/components/home/login-buttons.tsx
+++ src/components/home/login-buttons.tsx
@@ -1,28 +1,71 @@
-// app/home/LoginButtons.tsx
 "use client";
 
 import { signIn } from "next-auth/react";
 import GoogleIcon from '@mui/icons-material/Google';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 export default function LoginButtons() {
+  const [loadingGoogle, setLoadingGoogle] = useState(false);
+  const [loadingGitHub, setLoadingGitHub] = useState(false);
+
+  const handleGoogleLogin = async () => {
+    setLoadingGoogle(true);
+    try {
+      await signIn("google", { callbackUrl: "/chat" });
+    } catch (error) {
+      console.error("Erro ao fazer login com Google:", error);
+      setLoadingGoogle(false);
+    }
+  };
+
+  const handleGitHubLogin = async () => {
+    setLoadingGitHub(true);
+    try {
+      await signIn("github", { callbackUrl: "/chat" });
+    } catch (error) {
+      console.error("Erro ao fazer login com GitHub:", error);
+      setLoadingGitHub(false);
+    }
+  };
+
   return (
     <div className="flex flex-col gap-3 w-64">
       <Button
-        onClick={() => signIn("google", { callbackUrl: "/chat" })}
+        onClick={handleGoogleLogin}
         className="flex items-center justify-center gap-2 bg-white border border-zinc-300 text-zinc-950 hover:bg-zinc-100 rounded-xl py-2 cursor-pointer"
+        disabled={loadingGoogle}
       >
-        <GoogleIcon style={{ fontSize: 20 }} />
-        Entrar com Google
+        {loadingGoogle ? (
+          <div className="flex items-center gap-2">
+            <div className="w-4 h-4 border-2 border-t-transparent border-black rounded-full animate-spin" />
+            Entrando...
+          </div>
+        ) : (
+          <>
+            <GoogleIcon style={{ fontSize: 20 }} />
+            Entrar com Google
+          </>
+        )}
       </Button>
 
       <Button
-        onClick={() => signIn("github", { callbackUrl: "/chat" })}
+        onClick={handleGitHubLogin}
         className="flex items-center justify-center gap-2 bg-white border border-zinc-300 text-zinc-950 hover:bg-zinc-100 rounded-xl py-2 cursor-pointer"
+        disabled={loadingGitHub}
       >
-        <GitHubIcon style={{ fontSize: 20 }} />
-        Entrar com GitHub
+        {loadingGitHub ? (
+          <div className="flex items-center gap-2">
+            <div className="w-4 h-4 border-2 border-t-transparent border-black rounded-full animate-spin" />
+            Entrando...
+          </div>
+        ) : (
+          <>
+            <GitHubIcon style={{ fontSize: 20 }} />
+            Entrar com GitHub
+          </>
+        )}
       </Button>
     </div>
   );
